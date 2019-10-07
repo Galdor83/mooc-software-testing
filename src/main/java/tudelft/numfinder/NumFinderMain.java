dd5dd188999638eef2421f589bf2dd51d428a309
Alessandro Sironi <alessandro.sironi@welld.ch>
2019-10-07 18:10:36 +0200

    NumFinder

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -9,7 +9,7 @@ public class NumFinderMain {
         // nf.find(new int[] {4, 25, 7, 9});
 
         // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
+        nf.find(new int[] {4, 0, -3, 3, 2, 1, 5});
 
         System.out.println(nf.getLargest());
         System.out.println(nf.getSmallest());
